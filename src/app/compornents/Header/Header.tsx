6f1395f6799eb9da2c47af4bef987ca2313d9501
masato <masato19729@gmail.com>
2024-06-21 23:14:21 +0900

    🐛 シングルクォーテーション表記の修正

--- src/app/compornents/Header/Header.tsx
+++ src/app/compornents/Header/Header.tsx
@@ -3,7 +3,7 @@ import './Header.css';
 
 export function Header(): React.ReactElement {
     return (
-        <header className="sticky w-full h-10 top-0 bg-white pb-12">Masato's tech Blog
+        <header className="sticky w-full h-10 top-0 bg-white pb-12">{`Masato's tech Blog`}
             <div className="inline-flex float-right">
 
             </div>
