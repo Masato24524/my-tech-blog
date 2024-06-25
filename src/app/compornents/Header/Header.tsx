db90c86098e8b8a5d45b8b8cd968f44595d9bd11
masato <masato19729@gmail.com>
2024-06-25 14:46:49 +0900

    ✨ ハンバーガーメニューの追加、レスポンシブ対応の修正

--- src/app/compornents/Header/Header.tsx
+++ src/app/compornents/Header/Header.tsx
@@ -1,43 +1,89 @@
-import React, { useEffect, useState } from 'react';
+import { CSSProperties, useEffect, useState } from 'react';
 import './Header.css';
 import Link from 'next/link';
-// import { Router, useLocation } from 'react-router-dom';
 
 export function Header(): React.ReactElement {
-        // const location = useLocation();
+    const [menuOpen, setMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        const sidebar = document.querySelector('.sidebar') as HTMLElement;
+        const hamburgermenu = document.querySelector('.hamburgermenu') as HTMLElement;
+        if (menuOpen == false) {
+            sidebar.style.display = 'block'
+            hamburgermenu.style.display = 'none'
+            setMenuOpen(true);
+        } else {
+            sidebar.style.display = 'none'
+            hamburgermenu.style.display = 'block'
+            setMenuOpen(false);
+        };
+    }
 
-        // useEffect(() => {
-        //     if (typeof window !== 'undefined' && location.pathname === '/blogs' ) {
-        //         const bgDiv = document.createElement('div');
-        //         bgDiv.className = "bg-[url(https://picsum.photos/2000/200)]";
-        //         document.body.appendChild(bgDiv);
-    
-        //         return () => {
-        //             document.body.removeChild(bgDiv);
-        //         };
-        //     }
-        // }, [location.pathname]);
+    // const style: CSSProperties = {
+    //     display: 'hidden'
+    // }
 
     return (
         <div>
-            <header className='fixed w-full h-28 top-0 bg-white bg-center bg-cover font-medium bg-[url(/bg-main-cut.jpg)]'>
+            <header className='fixed z-0 w-full h-28 top-0 bg-white bg-center bg-cover font-medium bg-[url(/bg-main-cut.jpg)]'>
                 <Link href='/blogs' className='bg-white inline-flex p-1 px-2 m-2 rounded-lg bg-opacity-70'>
                     {`Masato's tech Blog`}
                 </Link>
-                <div className=' w-80 h-20 m-4 p-2 rounded-lg inline-flex float-right justify-center'>
-                    <Link href='/blogs' className='bg-white hover:bg-pink-200 flex m-auto p-1 px-8 mr-4 rounded-lg'>
-                       {'BLOG'}
-                    </Link>
-                    <Link href='#' className='bg-gray-300 flex m-auto p-1 px-8 mr-10 rounded-lg'>
-                        {'WORKS'}
-                    </Link>
-                </div>
 
+                <ul className='hidden lg:flex select-none w-80 h-20 m-4 p-2 rounded-lg float-right justify-center'>
+                    <li>
+                        {/* PCの場合のみ表示する要素 */}
+                        <Link href='/blogs' className='bg-white hover:bg-pink-200 flex m-auto p-1 px-8 mr-4 rounded-lg'>
+                            {'BLOG'}
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href='#' className='bg-gray-300 flex m-auto p-1 px-8 mr-10 rounded-lg'>
+                            {'WORKS'}
+                        </Link>
+                    </li>
+                </ul>
+                <Menu toggleMenu={toggleMenu} menuOpen={menuOpen} />
             </header>
-            {/* {showBackground && (
-                <div className='bg-[url(https://picsum.photos/2000/200)]'>
-                </div>
-            )}; */}
+        </div>
+    )
+}
+
+export const Menu = ({toggleMenu, menuOpen}: { toggleMenu: () => void, menuOpen: boolean}) => {
+    // const closeMenu = () => {
+    //     toggleMenu();
+    // }
+    
+    // useEffect(() => {
+    //     const hamburgermenu = document.querySelector('.hamburgermenu') as HTMLElement;
+    //     const sidebar = document.querySelector('.sidebar') as HTMLElement;
+    //     const handleTransitionEnd = () => {
+    //         if (menuOpen == true) {
+    //             hamburgermenu.style.display = 'none';
+    //             // sidebar.style.display = 'none';
+    //         };
+    //     };
+    //     sidebar.addEventListener('transitionend', handleTransitionEnd);
+
+    //     return () => {
+    //         sidebar.addEventListener('transitionend', handleTransitionEnd);
+    //     };
+    // }, [menuOpen])
+
+    return (
+        // スマホの場合のみ表示する要素
+        <div>
+            <img src='bars_24.svg' onClick={toggleMenu} className='hamburgermenu lg:hidden flex float-right z-20 w-12 h-12 mr-10 bg-white rounded-md'/>
+            <div className='sidebar hidden flex-col float-right text-center z-10 mr-10'>
+                <img src='bars_x.svg' onClick={toggleMenu} className='hamburger-menu-close lg:hidden float-right z-20 w-12 h-12 mb-2 bg-white rounded-md'/>
+                <ul className='lg:hidden'>
+                    <li className='w-20 h-auto mt-[55px] p-1 bg-white hover:bg-pink-200 border border-black'>
+                        <Link href='/blogs'>BLOG</Link>
+                    </li>
+                    <li className='mt-1 p-1 bg-gray-300'>
+                    <Link href='#'>WORKS</Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
