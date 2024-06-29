c7dcb491554912e468742f09c24335d458178105
masato <masato19729@gmail.com>
2024-06-29 19:43:41 +0900

    🎨 ハンバーガーメニューの表示、アニメーションを改善

--- src/app/compornents/Header/Header.tsx
+++ src/app/compornents/Header/Header.tsx
@@ -14,24 +14,25 @@ export const Header: React.FC = () => {
     
     return (
         <div>
-            <header className='fixed z-0 w-full h-28 top-0 bg-white bg-center bg-cover font-medium bg-[url(/bg-main-cut.jpg)]'>
-                <Link href='/blogs' className='bg-white inline-flex p-1 px-2 m-2 rounded-lg bg-opacity-70'>
+            <header id='header' className='fixed flex z-0 w-full h-28 top-0 bg-white bg-center bg-cover font-medium bg-[url(/OIG2-cut.jpg)]'>
+                <Link href='/blogs' className='bg-white h-8 p-1 px-2 m-2 rounded-lg bg-opacity-70'>
                     {`Masato's tech Blog`}
                 </Link>
+                <div className='flex-grow'></div>
+                    <ul className='hidden lg:flex h-20 m-4 p-2 rounded-lg '>
+                        <li className=''>
+                            {/* PCの場合のみ表示する要素 */}
+                            <Link href='/blogs' className='bg-white hover:bg-pink-200 flex m-auto p-1 px-8 mr-4 rounded-lg'>
+                                {'BLOG'}
+                            </Link>
+                        </li>
+                        <li className=''>
+                            <Link href='#' className='bg-gray-300 flex m-auto p-1 px-8 mr-10 rounded-lg'>
+                                {'WORKS'}
+                            </Link>
+                        </li>
+                    </ul>
 
-                <ul className='hidden lg:flex select-none w-80 h-20 m-4 p-2 rounded-lg float-right justify-center'>
-                    <li>
-                        {/* PCの場合のみ表示する要素 */}
-                        <Link href='/blogs' className='bg-white hover:bg-pink-200 flex m-auto p-1 px-8 mr-4 rounded-lg'>
-                            {'BLOG'}
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href='#' className='bg-gray-300 flex m-auto p-1 px-8 mr-10 rounded-lg'>
-                            {'WORKS'}
-                        </Link>
-                    </li>
-                </ul>
                 <Menu toggleMenu={toggleMenu} menuOpen={menuOpen} />
             </header>
         </div>
--- src/app/compornents/Menu/Menu.tsx
+++ src/app/compornents/Menu/Menu.tsx
@@ -11,20 +11,33 @@ type HeaderProps = {
 export const Menu: React.FC<HeaderProps> = ({ toggleMenu, menuOpen}) => {
     useEffect(() => {
         const hamburgermenu = document.querySelector('.hamburgermenu') as HTMLElement;
-        const sidebar = document.querySelector('.sidebar') as HTMLElement;
+        // const sidebar = document.querySelector('.sidebar') as HTMLElement;
+        const hamburgerMenuClose = document.querySelector('.hamburger-menu-close') as HTMLElement;
+        const sideMenu = document.querySelector('.sideMenu') as HTMLElement;
         
         const updateMenuDisplay = () => {
             if (window.innerWidth >= 1024) {
                 hamburgermenu.style.display = 'none'
-                sidebar.style.display = 'none';
+                hamburgerMenuClose.style.display = 'none'
+                // sidebar.style.display = 'none';
             } else {
                 if (!menuOpen) {
                     hamburgermenu.style.display = 'block'
-                    sidebar.style.display = 'none'
+                    // sidebar.style.display = 'none'
+                    hamburgerMenuClose.style.display = 'none'
+                    sideMenu.style.opacity = '0'
+                    // sideMenu.style.marginTop = '70px'
+                    sideMenu.style.transform = 'translateY(55px)'
                     // menuOpen = true;
                 } else {
                     hamburgermenu.style.display = 'none'
-                    sidebar.style.display = 'block'
+                    hamburgerMenuClose.style.display = 'block'
+                    // sideMenu.style.display = 'block'
+                    sideMenu.style.opacity = '100'
+                    // sideMenu.style.marginTop = '70px'
+                    sideMenu.style.transform = 'translateY(115px)'
+                    sideMenu.style.transitionDuration = '300ms'
+                    // mt-[70px] 
                     // setMenuOpen(false);
                 }
             }
@@ -41,19 +54,23 @@ export const Menu: React.FC<HeaderProps> = ({ toggleMenu, menuOpen}) => {
 
     return (
         // スマホの場合のみ表示する要素
-        <div>
-            <img src='../bars_24.svg' onClick={toggleMenu} className='hamburgermenu lg:hidden flex float-right z-20 w-12 h-12 mr-10 bg-white rounded-md'/>
-            <div className='sidebar hidden flex-col float-right text-center z-10 mr-10'>
-                <img src='../bars_x.svg' onClick={toggleMenu} className='hamburger-menu-close lg:hidden float-right z-20 w-12 h-12 mb-2 bg-white rounded-md'/>
-                <ul className='lg:hidden'>
-                    <li className='w-20 h-auto mt-[55px] p-1 bg-white hover:bg-pink-200 border border-black'>
+        // <div>
+
+            <div className="relative flex flex-col justify-end mb-4">
+                <ul className='sideMenu lg:hidden relative text-center h-16 z-20 w-28 mr-8 bg-gray-700 text-white'>
+                    <li className='p-1 hover:bg-pink-200 border-b'>
                         <Link href='/blogs'>BLOG</Link>
                     </li>
-                    <li className='mt-1 p-1 bg-gray-300'>
+                    <li className='p-1'>
                         <Link href='#'>WORKS</Link>
                     </li>
                 </ul>
+                <img src='../bars_24.svg' onClick={toggleMenu} className='hamburgermenu lg:hidden float-right ml-auto z-40 w-12 h-12 mr-8 bg-white rounded-md'/>
+                {/* <div className='sidebar hidden flex-col float-right text-center z-10 mr-10'> */}
+                <img src='../bars_x.svg' onClick={toggleMenu} className='hamburger-menu-close lg:hidden float-right ml-auto z-40 w-12 h-12 mr-8 bg-white rounded-md'/>
+
+                {/* </div> */}
             </div>
-        </div>
+        // </div>
     )
 }
