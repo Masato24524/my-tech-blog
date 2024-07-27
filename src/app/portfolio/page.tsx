0961e52a9f3665121acb5b07dabedda9d9bb9759
masato <masato19729@gmail.com>
2024-07-27 15:19:25 +0900

    🚸 ポートフォリオページのレイアウトを修正

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -7,15 +7,26 @@ const page = () => {
   return (
     <body>
         <Header />
-            <div>
-                <p className="p-40 pb-10 ">
-                【家計簿アプリ】
-                </p>
-                <Link href="https://masato24524.github.io/household-app-temp/">
-                    <img className="w-[35%] h-[35%] ml-40 mb-80" src="/household.png" alt="" />
-                </Link>
+            <div className="w-1/2 h-2/3 mt-40 mb-20 ml-20 rounded-lg shadow-md bg-white border-2 text-center">
+              <Link href="https://masato24524.github.io/household-app-temp/">
+                <img className="w-[100%] h-auto mb-4 p-4 pb-4" src="/household.png" alt="" />
 
+                <p className="pt-4 pb-2 font-bold bg-gray-100">
+                　　　　　　　家計簿アプリ💰（仮リリース版）<br />
+                </p>
+                <p className="pb-4 text-gray-600 bg-gray-100">
+                　日付ごとの収支が登録できる家計簿アプリです。<br />
+                　※スマホ画面のレイアウトは未対応<br />
+                </p>
+              </Link>
             </div>
+            <p className="pt-12 pb-20 pl-20 text-sm">
+                【注意点】<br />
+                　・本ページ内のアプリはポートフォリオ用のサンプルのため、データは予告なく削除される可能性があります。<br />
+                　 また、現在作成中のものがあり、仕様は予告なく変更される可能性があります。<br />
+                　・アプリの使用において生じた直接的、間接的、偶発的、特別または結果的損害<br />
+                　（利益の損失、データの喪失、業務の中断、コンピュータの故障など）について、当サイトは一切の責任を負いません。<br />
+                </p>
             <Footer />
 
     </body>
