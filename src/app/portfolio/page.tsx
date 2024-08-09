b4dc23ad4f6073aaf0df6668c3b58c5a257bb87f
masato <masato19729@gmail.com>
2024-08-09 16:17:49 +0900

    ✨ポートフォリオのページにNetflixクローンアプリを追加

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -8,19 +8,40 @@ const page = () => {
   return (
     <body>
         <Header />
-            <div className="w-1/2 h-2/3 mt-40 mb-20 ml-20 rounded-lg shadow-md bg-white border-2 text-center">
+          <div
+            id="container" 
+            className="flex w-[90%]"
+          >
+            <div 
+              id="household-app"
+              className="w-4/5 h-1/3 mt-40 mb-20 ml-20 rounded-lg shadow-md bg-white border-2 text-center">
               <Link href="https://masato24524.github.io/household-app-temp/">
-                <img className="w-[100%] h-auto mb-4 p-4 pb-4" src="/household.png" alt="" />
-
-                <p className="pt-4 pb-2 font-bold bg-gray-100">
-                　　　　　　　家計簿アプリ💰（仮リリース版）<br />
+                <img className="w-[400px] h-[300px] mb-4 p-8 pb-4" src="/household.png" alt="" />
+                <p className="pt-4 pb-2 font-bold bg-gray-200 text-center">
+                　　　　　家計簿アプリ💰（仮リリース版）<br />
                 </p>
-                <p className="pb-4 text-gray-600 bg-gray-100">
+                <p className="pb-4 text-gray-600 bg-gray-200">
                 　日付ごとの収支が登録できる家計簿アプリです。<br />
-                　※スマホ画面のレイアウトは未対応<br />
+                　※日付データの更新処理は未対応<br />
                 </p>
               </Link>
             </div>
+            <div 
+              id="netflix-clone-app"
+              className="w-4/5 h-1/3 mt-40 mb-20 ml-20 rounded-lg shadow-md bg-white border-2 text-center">
+              <Link href="https://netflix-clone-app-smoky.vercel.app/">
+                <img className="w-[400px] h-[300px] mb-4 p-8 pl-16 pb-4" src="/netflix-clone.png" alt="netflix-clone-app" />
+                <p className="pt-4 pr-4 pb-2 font-bold bg-gray-200 text-center">
+                　　　　　Nextflixクローンアプリ🎬（仮リリース版）<br />
+                </p>
+                <p className="pr-4 pb-4 text-gray-600 bg-gray-200">
+                　Netflixのサイト構成を模倣したアプリです。<br />
+                　映画をクリックすると、YouTubeのプロモーション映像が流れます。<br />
+                </p>
+              </Link>
+            </div>
+          </div>
+            
             <p className="pt-12 pb-20 pl-20 text-sm">
                 【注意点】<br />
                 　・本ページ内のアプリはポートフォリオ用のサンプルのため、データは予告なく削除される可能性があります。<br />
