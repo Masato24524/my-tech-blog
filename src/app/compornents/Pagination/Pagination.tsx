cc999373c6f14936a8205dbac529ab5754d9e26f
masato <masato19729@gmail.com>
2024-09-25 13:55:05 +0900

    🎨 ページネーションを実装し直した

--- src/app/compornents/Pagination/Pagination.tsx
+++ src/app/compornents/Pagination/Pagination.tsx
@@ -1,53 +1,57 @@
-'use client';
+"use client";
 
-import { getBlogs } from 'app/libs/client';
-import React, { useEffect, useState } from 'react'
+import { getBlogs } from "app/libs/client";
+import React, { useEffect, useState } from "react";
 
 type PaginationProps = {
-    totalPages: number;
-    currentPage: number;
-}
+  totalPages: number;
+  currentPage: number;
+};
 
 export const Pagination: React.FC<PaginationProps> = () => {
-    const [data, setData] = useState<{ totalCount: number, limit: number} | null>(null);
-    const [currentPage, setCrrentPage] = useState<number>(1);
+  const [data, setData] = useState<{
+    totalCount: number;
+    limit: number;
+  } | null>(null);
+  const [currentPage, setCrrentPage] = useState<number>(1);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const result = await getBlogs();
-            setData({
-                totalCount: result.totalCount,
-                limit: result.limit
-            });
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getBlogs();
+      setData({
+        totalCount: result.data.totalCount,
+        limit: result.data.limit,
+      });
+    };
 
-        fetchData();
-    }, []);
-    
-    if (!data) {
-        return <div>Loading...</div>;
-    }
+    fetchData();
+  }, []);
 
-    const totalPages = Math.ceil(data.totalCount / data.limit);
+  if (!data) {
+    return <div>Loading...</div>;
+  }
 
-    return (
-        <div>
-            {/* ページ番号の記載 */}
-            <div>
-                {Array.from({ length: totalPages }, (_, index) => (
-                <button className='bg-gray-300 px-2 m-5 mb-10 rounded-lg'
-                    key={index}
-                    onClick={() => {
-                    window.location.href = `/blogs?page=${index + 1}`;
-                    }}
-                    disabled={currentPage === index + 1}
-                >
-                    {index + 1}
-                </button>
-                ))}
-            </div>
-        </div>
-    )
-}
+  const totalPages = Math.ceil(data.totalCount / data.limit);
 
-export default Pagination
+  return (
+    <div>
+      {/* ページ番号の記載 */}
+      <div>
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            className="bg-gray-300 px-2 m-5 mb-10 rounded-lg"
+            key={index}
+            onClick={() => {
+              window.location.href = `/blogs?page=${index + 1}`;
+            }}
+            disabled={currentPage === index + 1}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
