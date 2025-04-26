95d12bf91a95158564a7a3b59b265134b278d860
masato <masato19729@gmail.com>
2025-04-26 16:10:06 +0900

    💄 codeタグにシンタックスハイライトを適用

--- src/app/utils/parse.tsx
+++ src/app/utils/parse.tsx
@@ -1,13 +1,38 @@
 "use client";
 
 import parse from "html-react-parser";
-import React from "react";
+import React, { useEffect } from "react";
+
+// import "prismjs/components/prism-javascript";
+// import "prismjs/components/prism-css";
+// import "prismjs/components/prism-jsx";
 
 // Parseコンポーネント
 const ParseHtml = ({ blogContent }: any) => {
+  // すべてのコードブロックにJavaScriptのクラスを適用
+  const enhancedHTML = blogContent.replace(
+    /<pre><code(?:\s+class="[^"]*")?>([^<]+)<\/code><\/pre>/g,
+    (match: any, code: any) => {
+      return `<pre class="language-javascript"><code class="language-javascript">${code}</code></pre>`;
+    }
+  );
+
+  // Prims.jsの初期化
+  useEffect(() => {
+    // Prism.jsをクライアントサイドのみインポート
+    const Prism = require("prismjs");
+
+    // 言語定義を明示的に読み込む
+    require("prismjs/components/prism-javascript");
+
+    // シンタックスハイライトを適用
+    Prism.highlightAll();
+  }, [enhancedHTML]); // 依存配列にenhancedHTMLを追加
+
   return (
     <div id="blog-doc" className="inline-block mb-10 pt-4">
-      {parse(blogContent)}
+      {parse(enhancedHTML)}
+      {/* {parse(blogContent)} */}
     </div>
   );
 };
